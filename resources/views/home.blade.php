aac92382571a26ceb4ece578e72493fb1feee228
AliceCaselli <alicecaselli3@gmail.com>
2023-05-10 22:48:42 +0200

    home content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,11 @@
 
 
 @section('content')
+    @foreach($comics as $comic)
+        {{$comic['title']}}
 
+            @foreach($comic['artists'] as $artist)
+                {{$artist}}
+            @endforeach
+    @endforeach
 @endsection
